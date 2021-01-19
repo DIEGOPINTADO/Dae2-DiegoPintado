37f320ff92c559029502e81f5f32569c3af9bcf1
Jose Ventura <ventura.jos3@gmail.com>
2021-01-18 22:14:44 -0500

    add 03 spring boot mvc

--- 03-spring-boot-mvc/src/main/java/pe/isil/controller/StudentController.java
+++ 03-spring-boot-mvc/src/main/java/pe/isil/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import pe.isil.model.Student;
 import pe.isil.service.StudentService;
 
@@ -25,14 +27,40 @@ public class StudentController {
 
     @GetMapping("/students")
     public String getAllStudents(Model model){
-
         List<Student> students = studentService.all();
         model.addAttribute("students", students);
-
-        log.info("students =>"+students);
-
         return "student";
     }
 
+    @GetMapping("/students/add")
+    public String addStudent(Model model){
+        model.addAttribute("student", new Student());
+        return "student-add";
+    }
+
+    @PostMapping("/students/save")
+    public String studentSave(Student student){
+        studentService.create(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{documentNumber}")
+    public String studentForUpdate(@PathVariable String documentNumber, Model model){
+        Student currentStudent = studentService.read(documentNumber);
+        model.addAttribute("student", currentStudent);
+        return "student-edit";
+    }
+
+    @PostMapping("/students/update")
+    public String studentUpdate(Student student){
+        studentService.update(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/delete/{documentNumber}")
+    public String studentDelete(@PathVariable String documentNumber){
+        studentService.delete(documentNumber);
+        return "redirect:/students";
+    }
 
 }
